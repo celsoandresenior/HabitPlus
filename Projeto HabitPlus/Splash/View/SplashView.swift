f2a2193b039681045cced4a9dd14e7c54d52dfcb
celso lima <celso.lima@evtit.com>
2022-11-28 14:05:39 -0300

    desenhando layout

--- Projeto HabitPlus/Splash/View/SplashView.swift
+++ Projeto HabitPlus/Splash/View/SplashView.swift
@@ -14,7 +14,16 @@ struct SplashView: View {
     var body: some View {
         switch state {
         case .loading:
-            Text("tela de loading...")
+            ZStack {
+                Image("aot")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding(20)
+                    .background(Color.black)
+                    .ignoresSafeArea()
+                    
+            }
         case .goToSignInScreen:
             Text("tela de sigIn...")
         case .goToHomeScreen:
