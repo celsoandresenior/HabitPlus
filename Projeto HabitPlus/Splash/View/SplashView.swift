a613076c11678a3109f58ed58f3919443b8944fd
celso lima <celso.lima@evtit.com>
2022-11-28 14:20:41 -0300

    terceira forma

--- Projeto HabitPlus/Splash/View/SplashView.swift
+++ Projeto HabitPlus/Splash/View/SplashView.swift
@@ -14,7 +14,15 @@ struct SplashView: View {
     var body: some View {
         switch state {
         case .loading:
-            LoadingView()
+            // 2. forma
+            //self.loading
+            
+            // 1 forma
+            //LoadingView()
+            
+            // 3. forma
+            self.loadingView(logo: "aot")
+            
         case .goToSignInScreen:
             Text("tela de sigIn...")
         case .goToHomeScreen:
@@ -25,10 +33,43 @@ struct SplashView: View {
     }
 }
 
+// 1. compartilhamento de objetos
 struct LoadingView: View {
     var body: some View {
         ZStack {
             Image("aot")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.black)
+                .ignoresSafeArea()
+                
+        }
+    }
+}
+
+// 2. extendendo a view e desenhandoa  tela.
+extension SplashView {
+    var loading: some View {
+        ZStack {
+            Image("aot")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.black)
+                .ignoresSafeArea()
+                
+        }
+    }
+}
+
+extension SplashView {
+    // suporta parametros.
+    func loadingView(logo: String) -> some View {
+        ZStack {
+            Image(logo)
                 .resizable()
                 .scaledToFit()
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
