34852f3925039111baaadeb8d217effb58848a40
celso lima <celso.lima@evtit.com>
2022-11-28 12:53:13 -0300

    first commit

---
+++ Projeto HabitPlus/Projeto_HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projeto_HabitPlusApp.swift
+//  Projeto HabitPlus
+//
+//  Created by Celso Lima on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Projeto_HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
